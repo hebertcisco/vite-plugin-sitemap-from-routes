490a0e1fd93f602069b82ad5e7aa133a8558a353
hebertcisco <hebertcisco@outlook.com>
2025-11-02 22:39:51 -0300

    chore: update jest.config.js to use ts-jest presets and improve ESM support

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const { defaultsESM } = require('ts-jest/presets');
+
 export default {
-  extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    '^.+\\.(t|j)s$': ['ts-jest', { useESM: true }],
+  ...defaultsESM,
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
